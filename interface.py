e22c785abca5f654b612c55376e7813871945d5b
sabi2007 <b16114ns@apps.ube-k.ac.jp>
2026-06-01 05:10:10 +0000

    interface.py:あなたはアシスタントですに変更

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたは詳細に答えるアシスタントです。"},
+    {"role": "system", "content": "あなたはアシスタントです。"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
